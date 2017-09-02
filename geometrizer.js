5b18a860a447086a4398dd7f1d2c5e0a8b416ae3
hardtechisnotacrime <balabekyan.alexander@gmail.com>
2017-09-02 22:23:58 +0300

    firt task

---
+++ geometrizer.js
@@ -0,0 +1,22 @@
+function calcCircumfrence(r) {
+	return 2 * Math.PI * r;
+}
+
+console.log("The circumference is " + calcCircumfrence(1));
+
+var a = 4234
+
+if (a != 0) {
+	adsfasdf
+}
+
+
+if (myAge >= 0) {
+	if (myAge <= 3) {
+ 		console.log('You\'re not quite in your peak.');
+ 	}
+}
+
+if (myAge > 90) {
+  console.log('You\'re not quite in your peak.');
+}
