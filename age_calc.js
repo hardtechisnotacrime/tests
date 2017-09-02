933bc0aebdf912b1874345cd64f001d392340091
hardtechisnotacrime <balabekyan.alexander@gmail.com>
2017-09-02 20:50:53 +0300

    firt task

---
+++ age_calc.js
@@ -0,0 +1,5 @@
+var yearOfBirth = 1990;
+var futureYear = 2072;
+var possibleAge1 = futureYear - yearOfBirth;
+var possibleAge2 = possibleAge1 - 1;
+console.log("I will be either " + possibleAge1 + " or " + possibleAge + " in " + futureYear);
