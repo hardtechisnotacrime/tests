fc24ee620bea2bdf96163b891aae1ccdd8c3d657
hardtechisnotacrime <balabekyan.alexander@gmail.com>
2017-09-02 20:27:45 +0300

    firt task

---
+++ first_test.js
@@ -0,0 +1,5 @@
+var numOfChildren = 17;
+var partName = "ANJELA";
+var geoLocation = "Uganda";
+var jobTitle = "manager";
+console.log("You will be a " + jobTitle + " in " + geoLocation + ", and married " + partName + " with " + numOfChildren + " kids.");
